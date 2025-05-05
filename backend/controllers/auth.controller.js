96110b20349ee2dbf9d103afbea1de1ab12a2e62
Bravinsimiyu <bravinwasike18@gmail.com>
2025-05-05 13:15:42 +0300

    1- project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,18 @@
+export const signup = async (req, res) => {
+    res.json({
+        data: "You have signed up successfully",
+    });
+}
+
+
+export const login = async (req, res) => {
+    res.json({
+        data: "You have signed up successfully",
+    });
+}
+
+export const logout = async (req, res) => {
+    res.json({
+        data: "You have signed up successfully",
+    });
+}
